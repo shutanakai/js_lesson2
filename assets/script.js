556ff8c74963b1bdac6242c9fe8371e44cafa578
t5a1r1ou <baw.keepon69@gmail.com>
2022-12-11 14:54:56 +0900

    learn function

--- assets/script.js
+++ assets/script.js
@@ -9,39 +9,53 @@ window.addEventListener("load", () => {
   // コンテナー
   const container = document.querySelector(".cardsContainer__mask");
 
+
+  // ボタンを押せるようにする
+  const enableButton = function (button) {
+    button.style.pointerEvents = "";
+    button.style.color = "#fff";
+  }
+
+  // ボタンを非活性にする
+  // ※アロー関数の参考
+  const disableButton = (button) => {
+    button.style.pointerEvents = "none";
+    button.style.color = "#555";
+  }
+
+  // カルーセルをスライドさせる
+  const slideCarousel = function (percent) {
+    container.style.transform = `transform(${percent})`;
+  }
+
   // Profileボタンを非活性にする
-  profileButton.style.pointerEvents = "none";
-  profileButton.style.color = "#555";
+  disableButton(profileButton);
 
   // Hobbyボタンを押した時
   hobbyButton.addEventListener("click", function () {
 
     // .cardsContainer__maskを-100%のところへ横に動かす
-    container.style.transform = "translateX(-100%)";
+    slideCarousel("-100%");
 
 
     // Hobbyボタンを非活性にする
-    hobbyButton.style.pointerEvents = "none";
-    hobbyButton.style.color = "#555";
+    disableButton(hobbyButton);
 
     // Profileボタンを押せるようにする
-    profileButton.style.pointerEvents = "";
-    profileButton.style.color = "#fff";
+    enableButton(profileButton);
   });
 
   // Profileボタンを押した時
   profileButton.addEventListener("click", function () {
 
     // .cardsContainer__maskを-0%のところへ横に動かす
-    container.style.transform = "translateX(0%)";
+    slideCarousel("0%");
 
     // Profileボタンを非活性にする
-    profileButton.style.pointerEvents = "none";
-    profileButton.style.color = "#555";
+    disableButton(profileButton);
 
     // Hobbyボタンを押せるようにする
-    hobbyButton.style.pointerEvents = "";
-    hobbyButton.style.color = "#fff";
+    enableButton(hobbyButton);
 
   });
 
