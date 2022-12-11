961fc1f3b5b0639fe7175ff2e98c95898db2e80a
t5a1r1ou <baw.keepon69@gmail.com>
2022-12-11 16:00:56 +0900

    learn array and for statement

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,8 @@ window.addEventListener("load", () => {
   // Profileボタン
   const profileButton = document.querySelector(".navigation__button--profile");
 
-  // Hobbyボタン
-  const hobbyButton = document.querySelector(".navigation__button--hobby");
-
-  // Birthplaceボタン
-  const birthplaceButton = document.querySelector(".navigation__button--birthplace");
+  // 全てのボタン
+  const triggerButtons = document.querySelectorAll(".navigation__button");
 
   // コンテナー
   const container = document.querySelector(".cardsContainer__mask");
@@ -17,68 +14,70 @@ window.addEventListener("load", () => {
   const enableButton = function (button) {
     button.style.pointerEvents = "";
     button.style.color = "#fff";
-  }
+  };
 
   // ボタンを非活性にする
   // ※アロー関数の参考
   const disableButton = (button) => {
     button.style.pointerEvents = "none";
     button.style.color = "#555";
-  }
+  };
 
   // カルーセルをスライドさせる
-  const slideCarousel = function (percent) {
+  const slideCarousel = function (index) {
+    const percent = `${-100 * index}%`;
     container.style.transform = `translateX(${percent})`;
-  }
+  };
 
   // ボタンを制御する
   const buttonControl = function(targetButton) {
 
-    // すべてのボタンを押せるようにする
-    enableButton(profileButton);
-    enableButton(hobbyButton);
-    enableButton(birthplaceButton);
+    for(let index = 0; index < triggerButtons.length; index++) {
+      const button = triggerButtons[index];
+      if (button === targetButton) {
+        disableButton(button);
+      } else {
+        enableButton(button);
+      };
+    };
 
-    // targetButtonを非活性にする
-    if (targetButton === profileButton) {
-      disableButton(profileButton);
-    } else if (targetButton === hobbyButton) {
-      disableButton(hobbyButton);
-    } else {
-      disableButton(birthplaceButton);
-    }
+    // 参考(forEach)
+    // triggerButtons.forEach((button) => {
+    //   if (button === targetButton) {
+    //     disableButton(button);
+    //   } else {
+    //     enableButton(button);
+    //   }
+    // });
   }
+
+  const clickButton = function(e) {
+
+    // e.targetでclickした対象を取得できる
+    const clickedButton = e.target;
+    let index;
+    for(let i = 0; i < triggerButtons.length; i++) {
+      const button = triggerButtons[i];
+      if (button === clickedButton) {
+        index = i;
+        break;
+      };
+    };
+
+    // 再帰関数参考(findIndex)
+    // const index = [...triggerButtons].findIndex((button) => button === clickedButton);
+
+    slideCarousel(index);
+    buttonControl(clickedButton);
+  };
 
   // Profileボタンを非活性にする
   disableButton(profileButton);
 
-  // Hobbyボタンを押した時
-  hobbyButton.addEventListener("click", function () {
+  triggerButtons.forEach(function(button) {
+    button.addEventListener("click", clickButton);
 
-    // .cardsContainer__maskを-100%のところへ横に動かす
-    slideCarousel("-100%");
-
-    // ボタンの制御
-    buttonControl(hobbyButton);
+    // 同じ意味
+    // button.addEventListener("click", (e) => clickButton(e));
   });
-
-  // Profileボタンを押した時
-  profileButton.addEventListener("click", function () {
-
-    // .cardsContainer__maskを-0%のところへ横に動かす
-    slideCarousel("0%");
-
-    // ボタンの制御
-    buttonControl(profileButton);
-  });
-
-  birthplaceButton.addEventListener("click", function() {
-
-    // .cardContainer__maskを-200%のところへ横に動かす
-    slideCarousel("-200%");
-
-    // ボタンの制御
-    buttonControl(birthplaceButton);
-  })
-
 });
